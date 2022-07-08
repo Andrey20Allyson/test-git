89a2b1dc0f7b9f00364cb2d09f90783d28181ab7
Andrey20Allyson <andreyuser47@gmail.com>
2022-07-07 23:19:55 -0300

    Update module_example.js

--- server_core/module_example.js
+++ server_core/module_example.js
@@ -1,7 +1,9 @@
 /**
  * @typedef {{
  * name: string;
- * sayHi(to: People): number;
+ * friends: Array<People>;
+ * say(message: String, to: People): void;
+ * startAFriendship(with_: People): void;
  * }} People
  */
 
@@ -12,9 +14,27 @@
 function CreatePeople(){ 
     return {
         name: 'hob',
+        friends: [],
 
-        sayHi(to){
-            console.log(`${this.name} says hi to ${to.name}`);
+        say(message, to){
+            var finalMessage = `${this.name} says ${message}`
+            if(to){
+                finalMessage += ` to ${to.name}!` 
+            }
+            console.log(finalMessage);
+        },
+
+        startAFriendshipWith(people){
+            if(this in people.friends){
+                people.friends.push(this)
+                this.friends.push(people)
+            }
+        },
+
+        sayToAllFriends(message){
+            for(let friend of this.friends){
+                console.log(`${this.name} says ${message} to ${friend}`);
+            }
         }
     }  
 }
